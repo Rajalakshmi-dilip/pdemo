01c385673e4e4a801ad6d6071821773bc22364ee
Rajalakshmi-dilip <rajalakshmi.d@optisolbusiness.com>
2025-05-06 12:35:04 +0530

    first commit

---
+++ playwright.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require('@playwright/test');
+
+module.exports = defineConfig({
+  testDir: './tests',
+  reporter: [
+    ['html', { outputFolder: 'playwright-report' }]
+  ],
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+    video: 'on',
+    screenshot: 'on',
+  },
+  timeout: 30000,
+  retries: 2,
+  workers: 4,
+});
---
+++ tests/login.spec.js
@@ -0,0 +1,10 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://ecommerce-admin.azureedge.net/login');
+  await page.getByTestId('email-input').click();
+  await page.getByTestId('email-input').fill('ecommerceadmin@yopmail.com');
+  await page.getByTestId('password-input').click();
+  await page.getByTestId('password-input').fill('Test@123');
+  await page.getByTestId('submit').click();
+});
